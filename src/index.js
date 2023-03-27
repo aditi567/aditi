0a4cc408acdcdd05da3390c84168dffaaedd11fc
Aditi Davange <91879778+aditi567@users.noreply.github.com>
2023-03-27 19:20:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,32 +8,6 @@ import Experience from './Experience';
 import blastoserve from './Blastoserve.mp4'
 import letter1 from './Experience_letter.jpg'
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if(entry.isIntersecting){
-      entry.target.classList.add('show');
-    } else{
-      entry.target.classList.remove('show');
-    }
-  });
-  });
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((el) => observer.observe(el));
- 
-const observer1 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if(entry.isIntersecting){
-      entry.target.classList.add('prof-aft-animi');
-    } else{
-      entry.target.classList.remove('prof-aft-animi');
-    }
-  });
-  });
-  const fadeInElements = document.querySelectorAll('.prof-animi');
-  fadeInElements.forEach((el) => observer1.observe(el));
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
